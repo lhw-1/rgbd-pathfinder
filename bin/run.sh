ad91c3c72b88554b4a52823809b5b9701cc0e9c6
lhw-1 <leehyungwoonsamuel@gmail.com>
2022-06-29 12:06:08 +0800

    Add Mask2Former data extraction

--- bin/run.sh
+++ bin/run.sh
@@ -17,7 +17,10 @@ eog rgbd-pathfinder/data/DPT_output/$FILENAME.png
 
 # Mask2Former Segmentation
 cp rgbd-pathfinder/input/$FILE Mask2Former/demo/$FILE
+cp -f rgbd-pathfinder/src/m2f_extraction/predictor.py Mask2Former/demo/predictor.py
+cp -f Mask2Former/demo/data.txt rgbd-pathfinder/data/M2F_output/data.txt
 cd Mask2Former/demo/
+rm data.txt
 # Change the parameters for --config-file and MODEL.WEIGHTS as needed here
 python demo.py --config-file ../configs/ade20k/panoptic-segmentation/maskformer2_R50_bs16_160k.yaml --input $FILE --output ../outputs/ --opts MODEL.WEIGHTS ../models/model_final_5c90d4.pkl
 cd ..
